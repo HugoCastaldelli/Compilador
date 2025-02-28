38fac4860cda6235a85476f7bc437dd3e5f4b4ef
Henrique Quintana <Henriquedantasquintana@gmail.com>
2025-02-28 10:55:22 -0300

    all reserved words

--- script.js
+++ script.js
@@ -70,17 +70,21 @@ function generateTableContent(code) {
     const table_content = [
         ["Lexema", "Token", "Linha", "Coluna Inicio", "Coluna Fim"]
     ];
-    
-    const tokens = {
-        "if": "palavra reservada",
-        "for": "palavra reservada",
-        "while": "palavra reservada",
-        ";": "final da linha"
-    };
 
+    const reservedWords = ["if", "else", "while", "do", "for", "ifelse", "case", "break", "int", "float", "double", "string", "char", "continue"];
+
+    const tokens = Object.fromEntries(
+        reservedWords.map(word => [word, "reserved word"])
+    );
+
+    tokens[";"] = "end of line";
+    tokens["("] = "open brackets";
+    tokens[")"] = "close brackets";
+    
     const reg_var = /^([a-zA-Z_][a-zA-Z0-9_]*)$/;
     const reg_int = /^(0|[1-9][0-9]*)$/;
     const reg_float = /^([0-9]+\.[0-9]+)$/;
+    const operator = /^(\+|\-|\*|\/)$/;
     
     const lines = code.split("\n");
     
@@ -99,9 +103,10 @@ function generateTableContent(code) {
                     tokenType = "float";
                 } else if (reg_var.test(word)) {
                     tokenType = "variável";
-                } else {
-                    tokenType = "Erro";
-                    
+                } else if(operator.test(word)){
+                    tokenType = "operator";   
+                } else{
+                    tokenType = "Error"
                 }
                 
                 const colEnd = colStart + word.length - 1;
